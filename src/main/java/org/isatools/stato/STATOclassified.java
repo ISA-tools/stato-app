7e1e83b5758fbf22524b6dac6d74e8a003c23c00
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-05-08 15:25:44 +0100

    More changes

--- src/main/java/org/isatools/stato/STATOclassified.java
+++ src/main/java/org/isatools/stato/STATOclassified.java
@@ -1,11 +1,11 @@
 package org.isatools.stato;
 
-import org.isatools.owl.ClassifiedOntology;
+import org.isatools.owl.DLQueryParser;
+import org.isatools.owl.OWLClassifier;
 import org.isatools.owl.OWLUtil;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import owltools.io.CatalogXmlIRIMapper;
 
 import java.io.File;
@@ -19,18 +19,28 @@ import java.io.IOException;
  *
  * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
  */
-public class STATOclassified extends ClassifiedOntology {
+public class STATOclassified  {
 
-    private String STATO_file = "/Users/agbeltran/work-dev/stato-agb/stato.owl";
-    private String classifiedSTATO_file =  "/Users/agbeltran/work-dev/stato-agb/classified-stato.owl";
+    OWLClassifier classifier = null;
+    OWLOntologyManager manager = null;
+    OWLOntology stato = null;
+
+    private DLQueryParser dlQueryParser = null;
+
+
+    private String STATO_file = "/Users/agbeltran/workspace/stato/src/ontology/stato.owl";
+    private String classifiedSTATO_file =  "/Users/agbeltran/workspace/src/ontology/stato/classified-stato.owl";
+    private IRI STATO_iri = IRI.create("http://purl.obolibrary.org/obo/stato.owl");
 
 
     public STATOclassified(){
 
         try {
-            loadLocalSTATO();
-            OWLOntology classifiedOntology = getClassifiedOntology();
-            OWLUtil.saveRDFXML(classifiedOntology, IRI.create("file:" + classifiedSTATO_file));
+
+            stato = loadLocalOntology(STATO_file);
+            classifier = new OWLClassifier();
+            classifier.classify(stato, STATO_iri);
+            dlQueryParser = new DLQueryParser(classifier.getManager());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,27 +51,42 @@ public class STATOclassified extends ClassifiedOntology {
     }
 
 
-    private void loadLocalSTATO()
+    private OWLOntology loadLocalOntology(String fileString)
             throws IOException, OWLOntologyCreationException {
-        File file = new File(STATO_file);
+        File file = new File(fileString);
         String path = file.getParent();
         String catalogPath = path + "/catalog-v001.xml";
         manager = OWLManager.createOWLOntologyManager();
         manager.addIRIMapper(new CatalogXmlIRIMapper(catalogPath));
-        ontology = manager.loadOntologyFromOntologyDocument(file);
-
+        return manager.loadOntologyFromOntologyDocument(file);
     }
 
 
+    private void runDLQuery(String dlQueryString){
+
+        OWLClassExpression classExpression = dlQueryParser.parse(dlQueryString);
+
+        NodeSet<OWLClass> set =classifier.getDescendants(classExpression);
+
+        System.out.println("Set =>" + set);
+    }
+
+
+    public void runQueries(){
+
+        for(String dlQuery: STATOQueries.QUERY_DL){
+
+            System.out.println("DL Query = "+dlQuery);
+            runDLQuery(dlQuery);
+        }
+
+    }
 
 
     public static void main(String[] args) {
 
         STATOclassified statoClassified = new STATOclassified();
-
-
-
-
+        statoClassified.runQueries();
     }
 
 }
