d1f1c09ac7bad1a506a7a30b6edbced7ed70f034
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-05-30 13:24:28 +0100

    Fixed queries

--- src/main/java/org/isatools/stato/STATOQueries.java
+++ src/main/java/org/isatools/stato/STATOQueries.java
@@ -21,11 +21,11 @@ public class STATOQueries {
     public static int QUERY_PLOTS_END = 11;
 
     public static String QUERY_MEASURES = "measures";
-    public static int QUERY_MEASURES_START = 12;
-    public static int QUERY_MEASURES_END = 12;
+    public static int QUERY_MEASURES_START = 11;
+    public static int QUERY_MEASURES_END = 11;
 
     public static String QUERY_DESIGN = "designs";
-    public static int QUERY_DESIGN_START = 13;
+    public static int QUERY_DESIGN_START = 12;
     public static int QUERY_DESIGN_END = 13;
 
 
@@ -37,10 +37,10 @@ public class STATOQueries {
         QUERY_STRING[0] = "Which statistical tests require the homoskedasticity hypothesis to be true in order to be used?";
         QUERY_DL[0] = "'statistical hypothesis test' and 'has part' some ('homoskedasticity hypothesis' and 'has value' value true)";
 
-        QUERY_STRING[1] = "Which statistical tests make particular assumptions before application?";
+        QUERY_STRING[1] = "Which statistical tests require specific hypotheses to be true in order to be used?";
         QUERY_DL[1] = "'statistical hypothesis test' and 'has part' some (hypothesis and 'has value' value true)";
 
-        QUERY_STRING[2] = "What are the statistical tests which can be used to test within subject comparison?";
+        QUERY_STRING[2] = "What are the statistical tests which can be used to perform within subject comparison?";
         QUERY_DL[2] = "'statistical hypothesis test' and achieves_planned_objective some 'within subject comparison objective'";
 
         QUERY_STRING[3] = "Which statistical tests use a contingency table?";
@@ -56,30 +56,30 @@ public class STATOQueries {
         QUERY_STRING[6] = "Which statistical test or data transformation method may be used to test association between categorical variable?";
         QUERY_DL[6] = "'statistical hypothesis test' and achieves_planned_objective some 'association testing objective'";
 
-        QUERY_STRING[7] = "Which statistical tests use 'normal distribution'?";
+        QUERY_STRING[7] = "Which statistical test use an ‘F- distribution'?";
         QUERY_DL[7] = "'statistical hypothesis test' and 'has part' some ('normal distribution hypothesis' and 'has value' value true)";
 
         //PLOTS
-        QUERY_STRING[8] = "Which plots may be produced in a genetic association study?";
+        QUERY_STRING[8] = "Which plots may be used to represent the results of a genetic association study?";
         QUERY_DL[8] = "graph and 'is about' some 'genetic association study'\n" +
                 "or\n" +
                 "is_specified_output_of some ('data visualization' and 'genetic association study')";
 
-        QUERY_STRING[9] = "Which plots may be produced in a meta-analysis?";
+        QUERY_STRING[9] = "Which plots may be used to represent the results of a meta-analysis?";
         QUERY_DL[9] = "is_specified_output_of some ('data visualization' and ('is preceded by' some 'meta analysis'))";
 
         QUERY_STRING[10] = "Which plots use effect size estimate?";
         QUERY_DL[10] = "graph and 'has part' some (axis and denotes some 'effect size estimate')";
 
-        QUERY_STRING[11] = "Which plots use effect size estimate?";
-        QUERY_DL[11] = "'study design' and declares some 'blocking variable'";
-
         //STATISTICAL MEASURES
-        QUERY_STRING[12] = "Which correlation coefficient can be used with exactly 2 ordinal variable?";
-        QUERY_DL[12] = "'correlation coefficient' and 'is about' exactly 2 'ordinal variable'";
+        QUERY_STRING[11] = "Which correlation coefficient can be used with exactly 2 ordinal variable?";
+        QUERY_DL[11] = "'correlation coefficient' and 'is about' exactly 2 'ordinal variable'";
 
         //STUDY DESIGN
-        QUERY_STRING[13] = "Which study designs use time as blocking variable?";
+        QUERY_STRING[12] = "Which study designs use blocking variables?";
+        QUERY_DL[12] = "'study design' and declares some 'blocking variable'";
+
+        QUERY_STRING[13] = "Which study designs uses time as blocking variable?";
         QUERY_DL[13] = "'study design' and declares some ('blocking variable' and 'is about' some 'time sampled measurement data set')";
 
     }
